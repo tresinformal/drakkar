b1317c8d13bdb0a685576033f9999161282ea110
swom <t.tiso@rug.nl>
2019-10-25 18:39:30 +0200

    the player has an initial speed of 0, set by default in constructor fix#45

--- player.h
+++ player.h
@@ -5,7 +5,7 @@
 class player
 {
 public:
-  player(const double x, const double y, const player_shape shape);
+  player(const double x, const double y, const player_shape shape, const double player_speed=0);
 
   /// Get the X coordinat of the player
   double get_x() const noexcept { return m_x; }
@@ -16,6 +16,9 @@ public:
   /// Get the shape of the player
   player_shape get_shape() const noexcept { return m_shape; }
   
+  /// Get the speed of the player
+  double get_speed() const noexcept { return m_player_speed; }
+
 private:
 
   /// The X coordinat of the player
@@ -26,6 +29,9 @@ private:
 
   ///The shape of the player
   player_shape m_shape;
+
+  /// The speed of the player
+  double m_player_speed;
 };
 
 /// Test the player class
