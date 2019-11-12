c927c591cee8c3f73b4efa7b99b3edb562e41dcc
swom <cicciacco@gmail.com>
2019-11-12 19:25:40 +0100

    fixing one complaint of OCLint regarding switch statement

--- action_type.cpp
+++ action_type.cpp
@@ -6,12 +6,12 @@ void test_action_type()
   assert(to_str_act_type(action_type::accelerate) != to_str_act_type(action_type::brake));
 }
 
-const std::string to_str_act_type(action_type this_action_type) //!OCLINT
+const std::string to_str_act_type(action_type this_action_type)
 //it might be wise to keep a default statement
 // in case for some reason an invalid input is given
 {
 
-    switch (this_action_type)
+    switch (this_action_type)//!OCLINT
 {
     case action_type::turn_left:   return "turn_left";
     case action_type::turn_right:   return "turn_right";
@@ -20,4 +20,5 @@ const std::string to_str_act_type(action_type this_action_type) //!OCLINT
 
     default:      return "[Unknown action_type]";
 }
+
 }
