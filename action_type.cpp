d2bab464b7b9e52e7fa85b71f01f787615d65b76
swom <cicciacco@gmail.com>
2019-11-12 18:25:50 +0100

    Fixing test for action_type to comply to OCLint standards. fix#68

--- action_type.cpp
+++ action_type.cpp
@@ -2,6 +2,19 @@
 #include<assert.h>
 void test_action_type()
 {
-  assert(action_type::turn_left != action_type::turn_right);
-  assert(action_type::accelerate != action_type::brake);
+  assert(to_str_act_type(action_type::turn_left) != to_str_act_type(action_type::turn_right));
+  assert(to_str_act_type(action_type::accelerate) != to_str_act_type(action_type::brake));
+}
+
+const std::string to_str_act_type(action_type this_action_type){
+
+    switch (this_action_type)
+{
+    case action_type::turn_left:   return "turn_left";
+    case action_type::turn_right:   return "turn_right";
+    case action_type::accelerate:   return "accelerate";
+    case action_type::brake:   return "brake";
+
+    default:      return "[Unknown action_type]";
+}
 }
--- action_type.h
+++ action_type.h
@@ -1,6 +1,8 @@
 #ifndef ACTION_TYPE_H
 #define ACTION_TYPE_H
 
+#include <string>
+
 enum class action_type
 {
   // Action types here
@@ -12,5 +14,6 @@ enum class action_type
 };
 
 void test_action_type();
+const std::string to_str_act_type(action_type this_action_type);
 
 #endif // ACTION_TYPE_H
