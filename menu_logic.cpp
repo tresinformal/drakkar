13318d54ee1e7b5d8dd0dde9d5bdd5ec92d91f4e
swom <t.tiso@rug.nl>
2019-12-02 18:33:53 +0100

    Created Menu, not yet implemented clicking but soon fix#74

--- menu_logic.cpp
+++ menu_logic.cpp
@@ -2,9 +2,9 @@
 
 
 menu_logic::menu_logic(unsigned int w_width, unsigned int w_height,
-           std::string button1,
-           std::string button2,
-           std::string button3):
+                       std::string button1,
+                       std::string button2,
+                       std::string button3):
     m_window_width{w_width},
     m_window_height{w_height},
     m_v_buttons{menu_button(button1),menu_button(button2),menu_button(button3)}
@@ -15,12 +15,10 @@ menu_logic::menu_logic(unsigned int w_width, unsigned int w_height,
 menu_button& menu_logic::get_button(unsigned int index)
 {
 
-   if(index < m_v_buttons.size()){
-       return m_v_buttons[index];
-   }
-   else{
-       return m_mock_button;
-   }
+    if(index < m_v_buttons.size()){
+        return m_v_buttons[index];
+    }
+    return m_mock_button;
 }
 
 
@@ -39,11 +37,11 @@ void test_menu(){
     //menu_view is built with values
     //for width and height of the screen
     {
-      unsigned int width = 1000;
-      unsigned int height = 1000;
-      menu_logic v(width,height);
-      assert(v.get_w_width() - width < 0.00000001);
-      assert(v.get_height() - height < 0.00000001);
+        unsigned int width = 1000;
+        unsigned int height = 1000;
+        menu_logic v(width,height);
+        assert(v.get_w_width() - width < 0.00000001);
+        assert(v.get_height() - height < 0.00000001);
     }
 
     //buttons are returned only for indexes
@@ -54,7 +52,7 @@ void test_menu(){
         menu_logic v;
         assert(v.get_button(0).get_name() == "action");
         assert(v.get_button(1000).get_name() == "INVALID INDEX!"
-                                                  " Trying to get a button that does not exist");
+                                                " Trying to get a button that does not exist");
     }
     //buttons are evenly distributed
     // along the height of the screen
@@ -66,7 +64,8 @@ void test_menu(){
         menu_logic v(width,height);
         v.put_buttons_tidy();
         for(unsigned int i = 0; i < v.get_m_v_buttons().size(); ++i)
-        assert(v.get_m_v_buttons()[i].get_y() - (i+1)*width/(v.get_m_v_buttons().size()+1) < 0.000001f );
+            assert(v.get_m_v_buttons()[i].get_y() -
+                   (i+1)*width/(v.get_m_v_buttons().size()+1) < 0.000001f );
     }
 
 }
