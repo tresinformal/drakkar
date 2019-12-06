43c663ef9efe722c7fd2889c477a5faaf5212706
AparajithaRamesh <a.ramesh@rug.nl>
2019-12-06 17:38:40 +0100

    fixed #56 and #59

--- environment_type.cpp
+++ environment_type.cpp
@@ -2,13 +2,27 @@
 #include "environment_type.h"
 #include <cassert>
 
+std::string to_str(environment_type this_env_type)
+{
+    switch (this_env_type)
+    {
+        case environment_type::empty:   return "empty";
+        case environment_type::quiet:   return "quiet";
+        case environment_type::random:   return "random";
+        case environment_type::attractive:   return "attractive";
+        case environment_type::repellent:   return "repellent";
+        case environment_type::wormhole:   return "wormhole";
+    }
+    return "[Unknown environment_type]";
+}
+
 void test_environment_type()
 {
   //Conversion to string
   {
-    assert(to_str_env_type(environment_type::empty) == "empty");
-    assert(to_str_env_type(environment_type::random)== "random");
-    assert(to_str_env_type(environment_type::random)!= "wormhole");
+    assert(to_str(environment_type::empty) == "empty");
+    assert(to_str(environment_type::random)== "random");
+    assert(to_str(environment_type::random)!= "wormhole");
   }
   //Other tests will follow here
 
