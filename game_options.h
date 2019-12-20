b8e7c51134d379757785836e7b31adfd245b298a
Neves-P <p.m.santos.neves@rug.nl>
2019-12-20 18:37:37 +0100

    Add game_options class. Fix #88

--- game_options.h
+++ game_options.h
@@ -2,6 +2,15 @@
 #define GAME_OPTIONS_H
 
 // Try to declare the class 'game_options' here yourself
+class game_options
+{
+public:
+  game_options(const bool play_music = true);
+  bool do_play_music() const noexcept { return m_play_music; }
+private:
+  bool m_play_music = true;
+};
+
 
 /// Test the 'game_options' class
 void test_game_options();
