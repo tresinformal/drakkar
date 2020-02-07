1887ec4d65169ea1d1b9eb88888f7a40d812141f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-07 18:24:19 +0100

    Expose #139

--- game.cpp
+++ game.cpp
@@ -420,6 +420,16 @@ void test_game() //!OCLINT tests may be many
     assert(!has_food_collision(g));
   }
   #endif // FIX_ISSUE_135
+  #ifdef FIX_ISSUE_139
+  //Can modify food items, for example, delete all food items
+  {
+    game g;
+    g.get_food();
+    assert(!g.get_food().empty());
+    g.get_food().clear();
+    assert(g.get_food().empty());
+  }
+  #endif //FIX_ISSUE_139
   #ifdef FIX_ISSUE_138
   // In the start of the game, there is no player-enemy collision
   {
@@ -427,4 +437,5 @@ void test_game() //!OCLINT tests may be many
     assert(!has_enemy_collision(g));
   }
   #endif // FIX_ISSUE_138
+  assert(1==2);
 }
