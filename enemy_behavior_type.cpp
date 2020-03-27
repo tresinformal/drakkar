0f1cb2c4765d279767503f2b6f4569ba179084c2
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-27 18:49:38 +0100

    enemy_behavior_type is actually tested

--- enemy_behavior_type.cpp
+++ enemy_behavior_type.cpp
@@ -1,9 +1,11 @@
 #include "enemy_behavior_type.h"
 
 #include <cassert>
+#include <vector>
+
 void test_enemy_behavior_type()
 {
-  enemy_behavior_type t = enemy_behavior_type::gezellig;
-  enemy_behavior_type u = enemy_behavior_type::shy;
+  const enemy_behavior_type t{enemy_behavior_type::gezellig};
+  const enemy_behavior_type u{enemy_behavior_type::shy};
   assert(t != u);
 }
