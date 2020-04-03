043bf9519db1123fe8d1092d30a086014dc30765
richelbilderbeek <richel@richelbilderbeek.nl>
2020-04-03 18:19:29 +0200

    Expose #156, #157, #159

--- enemy_behavior_type.cpp
+++ enemy_behavior_type.cpp
@@ -1,6 +1,7 @@
 #include "enemy_behavior_type.h"
 
 #include <cassert>
+#include <sstream>
 #include <vector>
 
 void test_enemy_behavior_type()
@@ -8,4 +9,11 @@ void test_enemy_behavior_type()
   const enemy_behavior_type t{enemy_behavior_type::gezellig};
   const enemy_behavior_type u{enemy_behavior_type::shy};
   assert(t != u);
+  //#define FIX_ISSUE_159
+  #ifdef FIX_ISSUE_159
+  {
+    std::stringstream s;
+    s << enemy_behavior_type::gezellig;
+  }
+  #endif //FIX_ISSUE_159
 }
