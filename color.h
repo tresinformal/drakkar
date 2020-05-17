44988503b589b569836e0498955d2bd44ac4e123
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-17 13:04:45 +0200

    Compile for C++11

--- color.h
+++ color.h
@@ -14,13 +14,13 @@ public:
   color(const int r = 255, const int g = 255, const int b = 255,
         const int a = 255);
 
-  constexpr int get_red() const noexcept { return m_r; }
-  constexpr int get_green() const noexcept { return m_g; }
-  constexpr int get_blue() const noexcept { return m_b; }
+  int get_red() const noexcept { return m_r; }
+  int get_green() const noexcept { return m_g; }
+  int get_blue() const noexcept { return m_b; }
 
   /// Get the opaqueness(from the color) of the shelter (0 = transparant, 255 =
   /// opaque)
-  constexpr int get_opaqueness() const noexcept { return m_a; }
+  int get_opaqueness() const noexcept { return m_a; }
 
 private:
   /// Redness, value from [0, 255]
--- environment.h
+++ environment.h
@@ -6,14 +6,14 @@ class environment
 {
 public:
   environment(double wall_short_side = 720, environment_type environment_type = environment_type::empty);
-  constexpr double get_min_x() const noexcept { return m_min_x; }
-  constexpr double get_min_y() const noexcept { return m_min_y; }
-  constexpr double get_max_x() const noexcept { return m_max_x; }
-  constexpr double get_max_y() const noexcept { return m_max_y; }
-  constexpr double get_wall_s_side() const noexcept { return m_wall_short_side;}
-  constexpr double get_wall_l_side() const noexcept { return m_wall_long_side;}
+  double get_min_x() const noexcept { return m_min_x; }
+  double get_min_y() const noexcept { return m_min_y; }
+  double get_max_x() const noexcept { return m_max_x; }
+  double get_max_y() const noexcept { return m_max_y; }
+  double get_wall_s_side() const noexcept { return m_wall_short_side;}
+  double get_wall_l_side() const noexcept { return m_wall_long_side;}
   /// Get environment type of the game
-  constexpr environment_type get_type() const { return m_environment_type; }
+  environment_type get_type() const { return m_environment_type; }
 
 private:
   double m_wall_short_side;
