b4d5b54d7f5e3c62edeafecd328a15943c97731a
swom <cicciacco@gmail.com>
2020-05-22 17:44:31 +0200

    working on #187

--- enemy_behavior_type.cpp
+++ enemy_behavior_type.cpp
@@ -8,7 +8,7 @@ void test_enemy_behavior_type()
 {
   const enemy_behavior_type t{enemy_behavior_type::gezellig};
   const enemy_behavior_type u{enemy_behavior_type::shy};
-  assert(t != u);
+  static_assert(t != u,"");
   {
     std::stringstream s;
     s << enemy_behavior_type::gezellig;
