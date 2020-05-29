3d7602cdc94b53789b34d8029c52757e15ace429
janclod <ctiecher@gmail.com>
2020-05-29 18:31:39 +0200

    Fix

--- game.cpp
+++ game.cpp
@@ -720,11 +720,5 @@ void test_game() //!OCLINT tests may be many
         assert(after-before > 0.0000000000000001);
       }
   }
-    // Test get functions
-    {
-        game g;
-        std::vector<food> f;
-        assert(g.get_food() == f);
-    }
 }
 
