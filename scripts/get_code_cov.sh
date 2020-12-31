4c9192fa10303beb58e7d84723ed62f80b252008
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-31 11:37:30 +0100

    Use subshell (instead of backtick) and a more precise regex

--- scripts/get_code_cov.sh
+++ scripts/get_code_cov.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-for filename in $(find . | egrep "\.cpp" | egrep -v "main\.cpp" | egrep -v "qrc_.*\.cpp"); 
-do
-  echo $filename
-  gcov -n -o . $filename; 
+for filename in $(find . | egrep '\.cpp$');
+do 
+  gcov -n -o . $filename > /dev/null; 
 done
