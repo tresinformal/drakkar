31e8990f71ceabb23d998b060bedb330f51fab0e
ollyturner <46792585+ollyturner@users.noreply.github.com>
2021-03-24 18:21:06 +0100

    Sorry I forgot to push #225

--- game.cpp
+++ game.cpp
@@ -105,6 +105,11 @@ void game::do_action( player& player, action_type action)
         player.shoot();
         break;
       }
+  case action_type::stun:
+  {
+    stun(player);
+    break;
+  }
     case action_type::none:
       return;
     }
