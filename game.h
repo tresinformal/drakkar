ee9726ead02f2f70f2d8561d2ad05543002aa0c8
swom <cicciacco@gmail.com>
2021-03-24 22:56:22 +0100

    adding tests for [FD] idea

--- game.h
+++ game.h
@@ -21,7 +21,7 @@ public:
        int num_players = 3,
        int n_ticks = 0,
        std::size_t n_shelters = 3,
-       int n_enemies = 0,
+       int n_enemies = 1,
        int n_food = 1);
 
   ///makes a player do an action
