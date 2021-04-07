7ba97b13963549ce81d9039a24d3c88eda1625a9
swom <cicciacco@gmail.com>
2021-04-07 19:28:21 +0200

    adding test for #261

--- food.cpp
+++ food.cpp
@@ -59,4 +59,12 @@ void test_food()
   }
 #endif
 
+#ifdef FIX_ISSUE_251
+  //Food has a regeneration timer member, set to 0 by default
+  {
+    food f;
+    assert(f.get_regeneration_timer() == 0);
+  }
+#endif
+
 }
