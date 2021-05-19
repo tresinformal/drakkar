d8ad0d558b8241a84a58060f7d4a2438fec04d7e
Pavel <mouseoskarx7@gmail.com>
2021-05-19 19:56:35 +0300

    continue working on issue 301 and 183

--- key_action_map.cpp
+++ key_action_map.cpp
@@ -144,11 +144,11 @@ void test_key_action_map()//!OCLINT tests can be many
   #ifdef FIX_ISSUE_282
   // operator==
   {
-    const key_action_map a = get_player_0_kam();
-    const key_action_map b = get_player_0_kam();
-    const key_action_map c = get_player_1_kam();
-    assert(a == b);
-    assert(b == a);
+    const key_action_map a = get_player_1_kam();
+    const key_action_map b = get_player_2_kam();
+    const key_action_map c = get_player_3_kam();
+    assert(!(a == b));
+    assert(!(b == a));
     assert(!(a == c));
     assert(!(b == c));
   }
