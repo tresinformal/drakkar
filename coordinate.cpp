4310b0f956ca2546b7ed3c0e8fec7d5db92a22f9
Pavel <mouseoskarx7@gmail.com>
2021-07-07 19:40:19 +0300

    fix develop

--- coordinate.cpp
+++ coordinate.cpp
@@ -1,9 +1,6 @@
 #include "coordinate.h"
 
-Coordinate::Coordinate()
-{
-
-}
+Coordinate::Coordinate(){}
 
 bool Coordinate::operator==(Coordinate in_coord) {
     return (m_x == in_coord.m_x) && (m_y == in_coord.m_y);
