32867f3c9aa481fa07a182a6e0d679e71f67091b
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-08-04 18:26:17 +0200

    wrote test for #356

--- coordinate.cpp
+++ coordinate.cpp
@@ -10,3 +10,13 @@ bool coordinate::operator!=(coordinate in_coord) {
     return not((m_x == in_coord.m_x) && (m_y == in_coord.m_y));
 }
 
+void test_coordinate() {
+  // Initial coordinates can be set at construction
+  // #define FIX_ISSUE_356
+  #ifdef FIX_ISSUE_356
+  {
+
+  }
+  #endif // FIX_ISSUE_356
+}
+
--- coordinate.h
+++ coordinate.h
@@ -2,16 +2,16 @@
 #define COORDINATE_H
 
 
-class Coordinate
+class coordinate
 {
 private:
     double m_x = 0;
     double m_y = 0;
 
 public:
-  Coordinate();
-  bool operator==(Coordinate in_coord);
-  bool operator!=(Coordinate in_coord);
+  coordinate();
+  bool operator==(coordinate in_coord);
+  bool operator!=(coordinate in_coord);
 };
 
 #endif // COORDINATE_H
