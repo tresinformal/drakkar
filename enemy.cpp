8c888e6350bef766060f0ad8a9551f95a9780db7
Theo Pannetier <t.s.c.pannetier@rug.nl>
2021-08-11 19:08:44 +0200

    fix #328

--- enemy.cpp
+++ enemy.cpp
@@ -1,6 +1,27 @@
 #include "enemy.h"
+#include "coordinate.h"
 #include <cassert>
 #include <sstream>
+
+enemy::enemy(const coordinate c)
+  : m_coordinate{c}
+{
+}
+
+std::stringstream& operator << (std::stringstream &out, const enemy &enemy)
+{
+    out << "Enemy(" << std::to_string(enemy.get_x()) <<
+           ", " << std::to_string(enemy.get_y()) <<
+           ")";
+
+    return out;
+}
+
+bool operator==(const enemy& lhs, const enemy& rhs) noexcept
+{
+    return lhs.get_x() == rhs.get_x()
+           && lhs.get_y() == rhs.get_y();
+}
 
 void test_enemy()
 {
@@ -14,7 +35,8 @@ void test_enemy()
   {
     const double x = 0.1;
     const double y = 0.3;
-    const enemy e{x, y};
+    const coordinate c{x, y};
+    const enemy e{c};
     assert(e.get_x() - x < 0.000001);
     assert(e.get_y() - y < 0.000001);
   }
@@ -32,31 +54,13 @@ void test_enemy()
     const enemy e;
     s << e;
   }
-    //#define FIX_ISSUE_328
+    #define FIX_ISSUE_328
     #ifdef FIX_ISSUE_328
     {
-      coordinate some_random_point(1,1);
+      coordinate some_random_point(1.0, 1.0);
       enemy n_enemy(some_random_point);
-      assert(n_enemy.get_position()==some_random_point);
+      assert(n_enemy.get_position() == some_random_point);
     }
     #endif
 #endif
 }
-
-enemy::enemy(const double x, const double y) : m_x{x}, m_y{y} {}
-
-std::stringstream& operator << (std::stringstream &out, const enemy &enemy)
-{
-    out << "Enemy(" << std::to_string(enemy.get_x()) <<
-           ", " << std::to_string(enemy.get_y()) <<
-           ")";
-
-    return out;
-}
-
-bool operator==(const enemy& lhs, const enemy& rhs) noexcept
-{
-    return lhs.get_x() == rhs.get_x()
-           && lhs.get_y() == rhs.get_y();
-}
-
