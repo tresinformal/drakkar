fff115670c8e5d9843b1ff03222f7f1d099b2746
kausmees <kristiina.ausmees@gmail.com>
2021-09-22 18:48:30 +0200

    Progress on #362

--- projectile.cpp
+++ projectile.cpp
@@ -5,8 +5,8 @@
 
 projectile::projectile(
   const coordinate c, const double direction, const projectile_type p,
-  const double radius, const std::string ownerID)
-  : m_coordinate{c}, m_direction{direction}, m_projectile_type{p}, m_radius{radius}
+  const double radius, const std::string& owner_id)
+  : m_coordinate{c}, m_direction{direction}, m_projectile_type{p}, m_radius{radius}, m_owner_id{owner_id}
 
 {
 }
@@ -22,6 +22,9 @@ void projectile::place(const coordinate& c)
 {
   m_coordinate = c;
 }
+
+std::string projectile::get_owner_id() noexcept { return m_owner_id;}
+
 
 double get_x(const projectile& p)  noexcept { return p.get_position().get_x();}
 double get_y(const projectile& p)  noexcept { return p.get_position().get_y();}
