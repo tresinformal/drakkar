8fd6c18f5f2aa5fa1f282386bf5298448c73f136
swom <cicciacco@gmail.com>
2021-09-29 18:17:14 +0200

    updating test #366

--- coordinate.cpp
+++ coordinate.cpp
@@ -1,5 +1,7 @@
 #include "coordinate.h"
+#include <cmath>
 #include <cassert>
+#include <vector>
 
 coordinate::coordinate(const double x, const double y)
   : m_x{x}, m_y{y}
@@ -10,26 +12,26 @@ double coordinate::get_x() const noexcept { return m_x; }
 double coordinate::get_y() const noexcept { return m_y; }
 
 bool coordinate::operator==(coordinate in_coord) {
-    return (m_x == in_coord.m_x) && (m_y == in_coord.m_y);
+  return (m_x == in_coord.m_x) && (m_y == in_coord.m_y);
 }
 
 bool coordinate::operator!=(coordinate in_coord) {
-    return not((m_x == in_coord.m_x) && (m_y == in_coord.m_y));
+  return not((m_x == in_coord.m_x) && (m_y == in_coord.m_y));
 }
 
 void test_coordinate() {
   // Initial coordinates can be set at construction
-  #define FIX_ISSUE_356
-  #ifdef FIX_ISSUE_356
+#define FIX_ISSUE_356
+#ifdef FIX_ISSUE_356
   {
     coordinate c{1.23456, 123456.789};
     assert(c.get_x() == 1.23456);
     assert(c.get_y() == 123456.789);
   }
-  #endif
+#endif
 
-  #define FIX_ISSUE_331
-  #ifdef FIX_ISSUE_331
+#define FIX_ISSUE_331
+#ifdef FIX_ISSUE_331
   {
     double x = 1.23456;
     double y = 123456.789;
@@ -39,35 +41,49 @@ void test_coordinate() {
     coordinate c3{x + 1.0, y};
     assert(c1 != c3);
   }
-  #endif
+#endif
 
-//#define FIX_ISSUE_366
+#define FIX_ISSUE_366
 #ifdef FIX_ISSUE_366
   {
     coordinate c{0,0};
-    double default_speed = 1;
+    ///Test along x axis
+    ///When direction is 0 player should
+    ///only move along the x axis
     double direction = 0;
+
+    double default_speed = 1;
     c.move(direction);
     assert(c.get_x() == default_speed);
     assert(c.get_y() == 0 );
 
-    c.reset_coords_to_zero();
-    double speed = 2;
-    c.move(direction, speed);
-    assert(c.get_x() == speed );
-    assert(c.get_y() == 0 );
+    std::vector<double> speeds{0.1,0.5,2,-0.1,-0.2,-1};
+    for(const auto& speed : speeds)
+      {
+        c.reset_coords_to_zero();
+        c.move(direction, speed);
+        assert(c.get_x() == speed );
+        assert(c.get_y() == 0 );
+      }
 
-    c.reset_coords_to_zero();
-    double speed = 0.5;
-    c.move(direction, speed);
-    assert(c.get_x() == speed );
-    assert(c.get_y() == 0 );
+    ///Test along y axis
+    ///When direction is 0 player should
+    ///only move along the x axis
+    direction = M_PI/2;
 
-    c.reset_coords_to_zero();
-    double speed = -1;
-    c.move(direction, speed);
-    assert(c.get_x() == speed );
-    assert(c.get_y() == 0 );
+    double default_speed = 1;
+    c.move(direction);
+    assert(c.get_x() == 0);
+    assert(c.get_y() == default_speed );
+
+    std::vector<double> speeds{0.1,0.5,2,-0.1,-0.2,-1};
+    for(const auto& speed : speeds)
+      {
+        c.reset_coords_to_zero();
+        c.move(direction, speed);
+        assert(c.get_x() == 0 );
+        assert(c.get_y() == speed );
+      }
   }
 #endif
 }
