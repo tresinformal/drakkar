b661a6d39956bc2ed510c37db1e7593efdcba3bd
janclod <ctiecher@gmail.com>
2022-05-25 18:40:30 +0200

    WIP #557

--- food_state.cpp
+++ food_state.cpp
@@ -1,13 +1,32 @@
 #include "food_state.h"
 #include "cassert"
+#include <sstream>
 
+std::ostream& operator << (std::ostream &out, const food_state &fs)
+{
+    return out << ;
+}
 
 
 void test_food_state()
 {
- #ifndef NDEBUG
- {
-   assert(food_state::eaten!= food_state::uneaten);
- }
- #endif
+#ifndef NDEBUG
+    {
+        assert(food_state::eaten!= food_state::uneaten);
+    }
+#define FIX_ISSUE_557
+#ifdef FIX_ISSUE_557
+    // to_str
+    {
+      assert(to_str(food_state::eaten) == "eaten");
+      assert(to_str(food_state::uneaten) == "uneaten");
+    }
+    {
+      const food_state food_state = food_state::eaten;
+      std::stringstream s;
+      s << food_state;
+      //assert(!s.str().empty());
+    }
+#endif // FIX_ISSUE_557
+#endif
 }
--- food_state.h
+++ food_state.h
@@ -1,11 +1,16 @@
 #ifndef FOOD_STATE_H
 #define FOOD_STATE_H
 
+#include <iostream>
 
 enum class food_state
 {
   eaten, uneaten
 };
 void test_food_state();
+
+/// Implement stream operator
+std::ostream& operator << (std::ostream &out, const food_state &fs);
+
 
 #endif // FOOD_STATE_H
