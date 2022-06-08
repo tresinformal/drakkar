e22dcd72165c9a2e1242ea3e3ae27acf6a8349ed
Theo Pannetier <t.s.c.pannetier@rug.nl>
2022-06-08 17:40:37 +0200

    test for #533

--- view_manager.cpp
+++ view_manager.cpp
@@ -1,4 +1,8 @@
 #include "view_manager.h"
+#include "game_view.h"
+#include "options_view.h"
+#include "menu_view.h"
+#include <cassert>
 
 view_manager::view_manager()
 {
@@ -13,6 +17,21 @@ void test_view_manager()
     {
         view_manager{};
     }
+
+    // (553) View manager has all view types
+    {
+      // #define FIX_ISSUE_533
+      #ifdef FIX_ISSUE_533
+      game_view gv;
+      menu_view mv;
+      options_view ov;
+      view_manager vw(gv, mv, ov);
+      assert(vw.get_game_view() == gv);
+      assert(vw.get_menu_view() == mv);
+      assert(vw.get_options_view() == ov);
+      #endif // FIX_ISSUE_533
+    }
+
 
   #endif
 }
