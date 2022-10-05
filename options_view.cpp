57bf70667f134c7ec54728517a3d54ef5fdcff0f
richelbilderbeek <richel@richelbilderbeek.nl>
2022-10-05 16:31:45 +0200

    Expose #631

--- options_view.cpp
+++ options_view.cpp
@@ -83,14 +83,12 @@ void options_view::show()
 #ifndef NDEBUG // no tests in release
 void test_options_view() //!OCLINT tests may be many
 {
-
-// (496) There should be a member of type view_mode
-{
-  options_view ov;
-  view_mode expected_next_view = view_mode::quit;
-  assert(ov.get_next_view() == expected_next_view);
-}
-
+  // (496) There should be a member of type view_mode
+  {
+    options_view ov;
+    view_mode expected_next_view = view_mode::quit;
+    assert(ov.get_next_view() == expected_next_view);
+  }
   // (545) An Options window doesn't open at construction
   {
     options_view ov;
@@ -99,6 +97,15 @@ void test_options_view() //!OCLINT tests may be many
     // and closing after, but that is not possible AFAICS
     // bc exec() doesn't exit on its own
   }
+  #ifdef FIX_ISSUE_631
+  // (631) An options_view allows a user to modify game_options
+  {
+    const game_options options;
+    const options_view view(options);
+    const game_options options_again(view.get_options());
+    assert(options == options_again);
+  }
+  #endif // FIX_ISSUE_631
 
 }
 #endif // NDEBUG // No tests in release
