31dbc8819e68e1c9a6254b2f442d15d78db161bd
Tianjian <57348932+EvoLandEco@users.noreply.github.com>
2022-11-02 20:44:59 +0100

    Delete utils.h

--- utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-/// Function to test the equality between two doubles
-bool are_equal(const double a, const double b, const double tolerance);
-
-void test_utils();
-
-#endif // UTILS_H
