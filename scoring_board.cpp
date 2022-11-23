d7cac11394d8daec4675efc85080a602d4949842
Tianjian <tianjianqin@outlook.com>
2022-11-23 17:52:06 +0100

    add more functionalities to scoring board

--- scoring_board.cpp
+++ scoring_board.cpp
@@ -18,6 +18,57 @@ scoring_board::scoring_board(int score_player1,
 
 }
 
+int scoring_board::get_score(int player) const noexcept
+{
+  switch (player)
+  {
+  case 0:
+    return m_score_player1;
+  case 1:
+    return m_score_player2;
+  case 2:
+    return m_score_player3;
+  default:
+    return 0;
+  }
+}
+
+void scoring_board::set_score(int player, int score)
+{
+  switch (player)
+  {
+  case 0:
+    m_score_player1 = score;
+    break;
+  case 1:
+    m_score_player2 = score;
+    break;
+  case 2:
+    m_score_player3 = score;
+    break;
+  default:
+    break;
+  }
+}
+
+void scoring_board::modify_score(int player, int amount)
+{
+  switch (player)
+  {
+  case 0:
+    m_score_player1 += amount;
+    break;
+  case 1:
+    m_score_player2 += amount;
+    break;
+  case 2:
+    m_score_player3 += amount;
+    break;
+  default:
+    break;
+  }
+}
+
 void scoring_board::set_winner(int winner)
 {
   if ((winner < 0) | (winner > 2))
@@ -33,9 +84,12 @@ void test_scoring_board()
 #ifndef NDEBUG // no tests in release
     #define FIX_ISSUE_542
     #ifdef FIX_ISSUE_542
-// When a scoring board is initialized players have 0 score and the time has not started yet
+    // When a scoring board is initialized players have 0 score and the time has not started yet
     {
         scoring_board sb;
+        // Can get a specific player's score
+        assert(sb.get_score(1) == 0);
+
         assert(sb.get_score_player1() == 0);
         assert(sb.get_score_player2() == 0);
         assert(sb.get_score_player3() == 0);
@@ -43,37 +97,69 @@ void test_scoring_board()
         assert(sb.get_winner() == -1);
     }
 
-    // Can set players' score, cannot set to negative value
+    // Can set players' score
     {
         scoring_board sb;
+
+        // Can get a specific player's score
+        sb.set_score(1, 500);
+        assert(sb.get_score(1) == 500);
+
         sb.set_score_player1(500);
         assert(sb.get_score_player1() == 500);
         sb.set_score_player2(500);
         assert(sb.get_score_player2() == 500);
         sb.set_score_player3(500);
         assert(sb.get_score_player3() == 500);
+    }
 
-        sb.set_score_player1(-500);
+    // Can set players' score
+    {
+        scoring_board sb;
+
+        // Can get a specific player's score
+        sb.set_score(1, 500);
+        assert(sb.get_score(1) == 500);
+
+        sb.set_score_player1(500);
         assert(sb.get_score_player1() == 500);
-        sb.set_score_player2(-500);
+        sb.set_score_player2(500);
         assert(sb.get_score_player2() == 500);
-        sb.set_score_player3(-500);
+        sb.set_score_player3(500);
         assert(sb.get_score_player3() == 500);
     }
 
-    // Can set timer, cannot set timer to negative value
+    // Can modify players' score
+    {
+        scoring_board sb;
+
+        // Can get a specific player's score
+        sb.set_score(1, 500);
+        sb.modify_score(1, 500);
+        assert(sb.get_score(1) == 1000);
+
+        sb.set_score(0, 500);
+        sb.modify_score_player1(500);
+        assert(sb.get_score_player1() == 1000);
+        sb.set_score(1, 500);
+        sb.modify_score_player2(500);
+        assert(sb.get_score_player2() == 1000);
+        sb.set_score(2, 500);
+        sb.modify_score_player3(500);
+        assert(sb.get_score_player3() == 1000);
+    }
+
+    // Can set timer
     {
         scoring_board sb;
         sb.set_timer(200);
         assert(sb.get_timer() == 200);
-        sb.set_timer(-200);
-        assert(sb.get_timer() == 200);
     }
 
-    // Can set winner, cannot set to value other than -1, 0, 1, 2
+    // Can set winner, cannot set to value other than 0, 1, 2
     {
         scoring_board sb;
-        assert(sb.get_winner() == -1);
+        assert(sb.get_winner() == 0);
         sb.set_winner(0);
         assert(sb.get_winner() == 0);
         sb.set_winner(1);
@@ -88,6 +174,8 @@ void test_scoring_board()
         assert(sb.get_winner() == -1);
     }
     #endif
+
+
 #endif
 }
 
--- scoring_board.h
+++ scoring_board.h
@@ -12,6 +12,8 @@ public:
                   int score_player3 = 0,
                   int timer = 0,
                   int winner = -1);
+    void set_score(int player, int score);
+
     void set_score_player1(int score) {m_score_player1 = score;};
 
     void set_score_player2(int score) {m_score_player2 = score;};
@@ -22,15 +24,25 @@ public:
 
     void set_winner(int winner);
 
+    int get_score(int player) const noexcept;
+
     int get_score_player1() const noexcept {return m_score_player1;};
 
-    int get_score_player2() {return m_score_player2;};
+    int get_score_player2() const noexcept {return m_score_player2;};
 
-    int get_score_player3() {return m_score_player3;};
+    int get_score_player3() const noexcept {return m_score_player3;};
 
-    int get_timer() {return m_timer;};
+    void modify_score(int player, int amount);
 
-    int get_winner() {return m_winner;};
+    void modify_score_player1(int amount) {m_score_player1 += amount;}
+
+    void modify_score_player2(int amount) {m_score_player2 += amount;}
+
+    void modify_score_player3(int amount) {m_score_player3 += amount;}
+
+    int get_timer() const noexcept {return m_timer;};
+
+    int get_winner() const noexcept {return m_winner;};
 
 private:
     int m_score_player1{0};
@@ -41,8 +53,7 @@ private:
 
     int m_timer{0};
 
-    // -1 indicates no winner
-    int m_winner{-1};
+    int m_winner{0};
 };
 
 void test_scoring_board();
