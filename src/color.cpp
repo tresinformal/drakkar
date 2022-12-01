c53c4bea65f4c9c38e949172c350df8cb1884896
Theo Pannetier <t.s.c.pannetier@rug.nl>
2022-12-01 14:52:35 +0100

    overwrite test for #628

--- src/color.cpp
+++ src/color.cpp
@@ -55,6 +55,15 @@ color create_black_color()
   return color(0, 0, 0, 255);
 }
 
+color get_random_respawn_color(std::mt19937& ran_num_gen) noexcept
+{
+    int r = 1 + ran_num_gen()/((ran_num_gen.max() + 1u)/255);
+    int g = 1 + ran_num_gen()/((ran_num_gen.max() + 1u)/255);
+    int b = 1 + ran_num_gen()/((ran_num_gen.max() + 1u)/255);
+
+    return color(r, g, b);
+}
+
 double calc_hue(const color &c)
 {
   double r = c.get_red()   / 255.0;
@@ -220,6 +229,31 @@ void test_color()
     assert(get_greenness(white) == 255);
     assert(get_blueness(white) == 255);
   }
+
+  #ifdef FIX_ISSUE_628
+  // (628) A random color between red, blue, green can be sampled
+  {
+    const color r = create_red_color();
+    const color g = create_green_color();
+    const color b = create_blue_color();
+
+    const int a_seed{42};
+    std::mt19937 rng1(a_seed);
+    const color a_color = get_random_respawn_color(rng1);
+    assert(a_color == r || a_color == g || a_color == b);
+
+    const int another_seed{43}; /// please change seed if draws same color as 42
+    std::mt19937 rng2(another_seed);
+    const color another_color = get_random_respawn_color(rng2);
+    assert(another_color == r || another_color == g || another_color == b);
+    assert(a_color != another_color);
+
+    const int same_seed = a_seed;
+    std::mt19937 rng3(same_seed);
+    const color same_color = get_random_respawn_color(rng3);
+    assert(same_color == a_color);
+  }
+#endif // FIX_ISSUE_628
 
 #endif // NDEBUG
 }
